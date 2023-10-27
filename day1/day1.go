af3fc69bbb1bcedce7bae3e7d4ddd4ca0c2102dc
AdeleRICHARD <adrichard@figarocms.fr>
2023-10-27 15:42:01 +0200

    Add day1

---
+++ day1/day1.go
@@ -0,0 +1,45 @@
+package day1
+
+import (
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func Day1() {
+	// Read calories.txt
+	// For each values, calculate the calories
+	// Add to a table the number of calories for each void in the file
+	// Print the number of calories for the most calories
+	txtCalories, err := os.ReadFile("day1/calories.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	caloriesToCount := strings.Split(string(txtCalories), "\n")
+	calories := make([]int, len(caloriesToCount))
+	index := 0
+	for _, calorie := range caloriesToCount {
+		if calorie == "" {
+			index++
+			continue
+		}
+		calorieInt, err := strconv.Atoi(calorie)
+		if err != nil {
+			panic(err)
+		}
+
+		calories[index] += calorieInt
+	}
+	slices.Max(calories)
+	println("max calories : ", calories[0])
+
+	var top3Sum int
+
+	for _, calorie := range calories[:3] {
+		top3Sum += calorie
+	}
+	println("top 3 des elfs qui ont le plus de calories : ", top3Sum)
+
+}
