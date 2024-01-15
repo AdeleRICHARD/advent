6cf1902656a94c373d7004cb0106ac9abca6d545
AdeleRICHARD <adrichard@figarocms.fr>
2024-01-15 17:54:16 +0100

    Parsing ok

--- day11/day11.js
+++ day11/day11.js
@@ -1,21 +1,41 @@
 // Read the file first
 const fs = require('fs');
+const getOperation = (operation) => {
+    operation = operation.match(/([+\-*/])\s+(\d+|old)/);
+    if(operation){
+        return operation.slice(1);
+    } else {
+       console.log("No operation found");
+    }
+}
+
+const getNumberInString = (string) => {
+    let numbers = string.match(/\d+(,\s*\d+)*/g);
+    if(numbers){
+        return numbers[0];
+    } else {
+        console.log("No number found");
+    }
+}
 const input = fs.readFileSync('./Monkeys.txt', 'utf-8').split('\n\n');
 
 // Part 1
 // Parse each monkey action into an object
-// use pop ?
 const monkeys = input.map((line) => {
+    if(line === ''){
+        return;
+    }
     const action = line.split('\n').map(line => line.trim());
-    console.log(action[0]);
+    
     return{
-        monkeyId : action[0].split(' ')[1],
-        startingItems : action[1].split(':')[1].trim().split(','),
-        operation : action[2].split('=')[1].trim().split('+'),
-        test : action[3].split(':')[1].trim().split(' '),
-        trueTest : action[4].split(':')[1].split(' '),
-        falseTest : action[5].split(':')[1].split(' '),
+        monkeyId : getNumberInString(action[0]),
+        startingItems : getNumberInString(action[1]),
+        operation : getOperation(action[2]),
+        test : getNumberInString(action[3]),
+        trueTest : getNumberInString(action[4]),
+        falseTest : getNumberInString(action[5]),
     };
 });
+
 
 console.log(monkeys);
